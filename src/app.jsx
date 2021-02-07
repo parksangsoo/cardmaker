640650c7231c334cd6777ef83a11e8df4d841d23
parksangsoo <pssw0728@gmail.com>
2021-02-07 19:25:32 +0900

    cardmaker first commit

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,22 @@
-import './app.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import styles from './app.module.css';
+import Login from './components/login/login';
+import Maker from './components/maker/maker';
 
-function App() {
+function App({ authService }) {
   return (
-    <div className="App">
-      hello
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
